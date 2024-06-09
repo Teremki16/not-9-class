1d78fa9477108827e5a6abf41381443723343824
Teremki16 <teremki16robocode@gmail.com>
2024-06-09 15:49:00 +0300

    лисач зроили кнопку і додавання нотаток доробити кнопки

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
 let notes = []
 
-$(document).ready(function () {
+if(localStorage.getItem("notes")!=null){
+  notes = JSON.parse(localStorage.getItem("notes"))
+  draw()
+}
+
+
   $("#add").on("click", function(){
     let note = {
       title:$("#head").val(),
       text:$("#text").val(),
-      data:new Date().getTime()
+      data:new Date().getTime(),
+      complete: false
     }
     $("#head").val("")
     $("#text").val("")
     notes.push(note)
     console.log(notes)
+    draw()
   })
 
+function draw(){
+  localStorage.setItem("notes",JSON.stringify(notes))
+  $(".notes").empty();
+  notes.forEach((msg)=>{
+    $(".notes").append(`
+      <div class="note ${msg.complete}">
+      <div class="zagolovok">${msg.title}</div>
+      <ul>
+      <li><div class="text">${msg.text}</div></li>
+      </ul>
+      <div class="data">${new Date(msg.data).toDateString()}</div>
+      <div class="knopka">
+        <button onclick="del(${msg.data})"><i class="fa-solid fa-trash-can"></i></button>
+        <button><i class="fa-solid fa-pencil"></i></button>
+        <button><i class="fa-solid fa-check"></i></button>
+      </div>
+    </div>
+      `)
+  })
+}
+
+$("#clear").click(()=>{
+  notes = []
+  draw()
+})
+
+
+function del(id){
+  notes = notes.filter(item => item.data !== id);
+  draw();
+}
 
 
 
+if (objectToModify) {
+  objectToModify.complete = objectToModify.complete
+}
+
+ draw()
+
+ 
+  function redact(data) {
+    let objectToModify = notes.find(obj => obj.data == data);
+  
+
+if(objectToModify) {
+ objectToModify.title = prompt
+}
+}
+
+function complete(data) {
+  let objectToModify = notes.find(obj => obj.data == data);
 
 
 
+if(objectToModify) {
+  objectToModify.complete= !objectToModify.complete
+  draw()
 
 
-
-});
-
+}
+}
 
 
 // new Date(data).toDateString()
@@ -31,3 +89,7 @@ $(document).ready(function () {
 // localStorage.setItem("notes", JSON.stringfy(data))
 
 // JSON.parse(localStorage.getItem("notes"))
+
+
+
+
