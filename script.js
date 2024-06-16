d6a05e09823d9f391f520a4d5a571958c93a7776
Teremki16 <teremki16robocode@gmail.com>
2024-06-16 15:47:02 +0300

    Co-authored-by: anngols <anngols@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let notes = []
+let last;
 
-if(localStorage.getItem("notes")!=null){
+if (localStorage.getItem("notes") != null) {
   notes = JSON.parse(localStorage.getItem("notes"))
   draw()
 }
 
 
-  $("#add").on("click", function(){
+$("#add").on("click", function () {
+  if ($("#head").val() != "" && $("#text").val() != "") {
+
     let note = {
-      title:$("#head").val(),
-      text:$("#text").val(),
-      data:new Date().getTime(),
+      title: $("#head").val(),
+      text: $("#text").val(),
+      data: new Date().getTime(),
       complete: false
     }
     $("#head").val("")
@@ -18,12 +21,18 @@ if(localStorage.getItem("notes")!=null){
     notes.push(note)
     console.log(notes)
     draw()
-  })
 
-function draw(){
-  localStorage.setItem("notes",JSON.stringify(notes))
+  }
+  else {
+    alertify.error("заповніть усі поля")
+
+  }
+})
+
+function draw() {
+  localStorage.setItem("notes", JSON.stringify(notes))
   $(".notes").empty();
-  notes.forEach((msg)=>{
+  notes.forEach((msg) => {
     $(".notes").append(`
       <div class="note ${msg.complete}">
       <div class="zagolovok">${msg.title}</div>
@@ -33,55 +42,64 @@ function draw(){
       <div class="data">${new Date(msg.data).toDateString()}</div>
       <div class="knopka">
         <button onclick="del(${msg.data})"><i class="fa-solid fa-trash-can"></i></button>
-        <button><i class="fa-solid fa-pencil"></i></button>
-        <button><i class="fa-solid fa-check"></i></button>
+        <button  onclick="redact(${msg.data})"><i class="fa-solid fa-pencil"></i></button>
+        <button onclick="complete(${msg.data})"><i class="fa-solid fa-check"></i></button>
       </div>
     </div>
       `)
   })
 }
 
-$("#clear").click(()=>{
+$("#clear").click(() => {
   notes = []
   draw()
 })
 
 
-function del(id){
+function del(id) {
   notes = notes.filter(item => item.data !== id);
   draw();
 }
 
-
-
-if (objectToModify) {
-  objectToModify.complete = objectToModify.complete
+function redact(data) {
+  let objectToModify = notes.find(obj => obj.data == data);
+  openOverlay()
+  if (objectToModify) {
+    // objectToModify.title = prompt("введіть новий заголовок")
+    // objectToModify.text = prompt("введіть нову нотатку")
+    $("#redtitle").val(objectToModify.title)
+    $("#redtext").val(objectToModify.text)
+    last = data
+  }
+draw()
 }
 
- draw()
+function saveNote(){
+  let objectToModify = notes.find(obj => obj.data == last);
+  objectToModify.title = $("#redtitle").val()
+  objectToModify.text = $("#redtext").val()
+  closeOverlay()
+  draw()
 
- 
-  function redact(data) {
-    let objectToModify = notes.find(obj => obj.data == data);
-  
-
-if(objectToModify) {
- objectToModify.title = prompt
-}
 }
 
 function complete(data) {
   let objectToModify = notes.find(obj => obj.data == data);
+  if (objectToModify) {
+    objectToModify.complete = !objectToModify.complete;
+    draw()
 
-
-
-if(objectToModify) {
-  objectToModify.complete= !objectToModify.complete
-  draw()
-
-
+  }
 }
+
+function openOverlay() {
+  document.getElementById('overlay').style.visibility = 'visible';
 }
+
+function closeOverlay() {
+  document.getElementById('overlay').style.visibility = 'hidden';
+}
+
 
 
 // new Date(data).toDateString()
